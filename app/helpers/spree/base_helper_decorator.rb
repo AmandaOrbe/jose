3c1ddee50c8e22d3d3297a14f46a621c79e80993
(Amanda Orbe) <garayomiriam@gmail.com>
2018-07-10 16:05:17 +0200

    navbar

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,12 +1,11 @@
 module Spree::BaseHelper
     def pretty_time(time, format = :long)
-
          I18n.l(@order.completed_at)
     end
 
     def nav_tree(root_taxon, current_taxon, max_level = 1)
       return '' if max_level < 1 || root_taxon.children.empty?
-      content_tag :div, class: "#{root_taxon.name} taxonomies-list__taxonomy__subtaxons list-inline" do
+      content_tag :div, class: "t_#{root_taxon.taxonomy.position} taxonomies-list__taxonomy__subtaxons list-inline" do
         taxons = root_taxon.children.map do |taxon|
           css_class = ( current_taxon && current_taxon.self_and_ancestors.include?(taxon) && "navbar-item") ? 'active' : nil
           content_tag :h5, class:  css_class do
