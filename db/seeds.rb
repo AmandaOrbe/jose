fde4f9336328e1704db90efdf1376c77d75a7ce6
(Amanda Orbe) <garayomiriam@gmail.com>
2018-07-19 11:32:00 +0100

    added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+
+puts 'Creating products...'
+tour_d_argent = Spree::Product.new(name: "La Tour d'Argent")
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(name: "Chez Gladines")
+chez_gladines.save!
+puts 'Finished!'
