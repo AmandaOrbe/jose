18c7dea8edf3b7275c528e0da6a3cc83e9cddae7
(Amanda Orbe) <garayomiriam@gmail.com>
2018-08-24 16:28:45 +0200

    search improved

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -3,18 +3,24 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       if @searcher.keywords
+        @all_products = Spree::Product.all
         @products = []
         @best_match= []
-        Spree::Product.all.each do |product|
+        @all_products.each do |product|
           if product.name.downcase.include? @searcher.keywords
             @best_match << product
           end
+           @all_products.reject{|product|}
+        end
 
-          @searcher.keywords.split.each do |word|
-            if product.name.downcase.include? word.downcase
-              @products <<  product
-            elsif  product.description.downcase.include? word.downcase
-              @products <<  product
+        unless @best_match.first
+          @all_products.each do |product|
+            @searcher.keywords.split.each do |word|
+              if product.name.downcase.include? word.downcase
+                @products <<  product
+              elsif  product.description.downcase.include? word.downcase
+                @products <<  product
+              end
             end
           end
         end
